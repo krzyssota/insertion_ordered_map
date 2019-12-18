930bf0b0b40e1a0c0bb8fa3fd3f1eaa7f861cdba
Bartosz Smoczynski <b.smoczynski@student.uw.edu.pl>
2019-12-18 11:00:52 -0500

    sam nie wiem co

--- insertion_ordered_map.h
+++ insertion_ordered_map.h
@@ -33,7 +33,14 @@ class map_buffer {
     refs(1), unsharable(false) {
 
     }
-    map_buffer &operator=(const map_buffer &) = delete;
+    map_buffer &operator=(const map_buffer & other) {
+        this->first = other.first;
+        this->last = other.last;
+        this->buf = buf(other.buf);
+        this->refs = 1;
+        this->unsharable = false;
+        return *this;
+    };
     map_buffer(map_buffer<K, V, Hash> &&other) noexcept {
         buf = move(other.buf);
         first = other.first;
@@ -46,28 +53,28 @@ class map_buffer {
 template<class K, class V, class Hash = std::hash<K>>
 class insertion_ordered_map {
 
-    shared_ptr<map_buffer<K, V, Hash>> data;
-    void about_to_modify(bool mark_unshareable = false) {
-        if(data->refs > 1 && !data->unsharable) {
-            auto* new_data = new map_buffer<K, V, Hash>(data); // ?new_data(data)?
-            --data->refs;
-            data = new_data;
+    shared_ptr<map_buffer<K, V, Hash>> data_ptr;
+    void about_to_modify(bool mark_unsharable = false) {
+        if(data_ptr->refs > 1 && !data_ptr->unsharable) {
+            auto* new_data = new map_buffer<K, V, Hash>(data_ptr); // ?new_data(data_ptr)?
+            --data_ptr->refs;
+            data_ptr = new_data;
         } else {
             // chyba nic bo unordered_map samo się powiekszy
         }
-        if(mark_unshareable){
-            data->unsharable = true;
+        if(mark_unsharable){
+            data_ptr->unsharable = true;
         }
         else {
-            data->refs = 1;
+            data_ptr->refs = 1;
         }
     }
 
 public:
     V &operator[](K const &k){
         about_to_modify(true);
-        auto *it = data->buf.find(k);
-        if(*it != data->buf.end()){
+        auto it = data_ptr->buf.find(k);
+        if(it != data_ptr->buf.end()){
             //...
             return it->second.value;
         } else {
@@ -81,18 +88,19 @@ public:
     złożoność czasowa O(1) lub oczekiwana O(n), jeśli konieczne jest wykonanie kopii.
     insertion_ordered_map(insertion_ordered_map const &other);*/
     insertion_ordered_map<K, V, Hash>(const insertion_ordered_map<K,V,Hash>& other) {
-        if(other.data->unsharable) {
-            data = new map_buffer<K, V, Hash>(other.data);
+        if(other.data_ptr->unsharable) {
+            data_ptr = new map_buffer<K, V, Hash>(other.data_ptr);
         } else {
-            data = other.data;
-            ++data->refs;
+            data_ptr = other.data_ptr;
+            ++data_ptr->refs;
         }
     }
-    Fibo(Fibo&& that) noexcept : fibset(std::move(that.fibset)) {};
-    insertion_ordered_map(insertion_ordered_map &&other) noexcept {
-        data(move(other.data));
+
+    insertion_ordered_map(insertion_ordered_map &&other) noexcept
+    {
+        data_ptr = make_shared<map_buffer<V, K, Hash>>(move(*other.data_ptr));
     }
-}
+};
 
 
 #endif //INSERTION_ORDERED_MAP_INSERTION_ORDERED_MAP_H
