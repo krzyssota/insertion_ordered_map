a887352a1c29b9bed37987d8c441713301c46f10
ks406362 <k.sota@student.uw.edu.pl>
2019-12-18 13:29:49 +0100

    first draft of structure

--- insertion_ordered_map.h
+++ insertion_ordered_map.h
@@ -1,10 +1,38 @@
-//
-// Created by krzubuntu on 16.12.2019.
-//
-
 #ifndef ISERTION_ORDERED_MAP_INSERTION_ORDERED_MAP_H
 #define ISERTION_ORDERED_MAP_INSERTION_ORDERED_MAP_H
 
-//pleple
+#include <iostream>
+#include <unordered_map>
 
-#endif //ISERTION_ORDERED_MAP_INSERTION_ORDERED_MAP_H
+
+using namespace std;
+
+template<typename V>
+struct list_entry {
+    V value;
+    list_entry<V> *next;
+    list_entry<V> *previous;
+};
+
+template<class K, class V, class Hash = std::hash<K>>
+class map_buffer {
+    unordered_map<K, list_entry<V>, Hash> buf;
+    list_entry<V> *first;
+    list_entry<V> *last;
+
+    public :
+
+    map_buffer(const map_buffer &) = delete;
+    map_buffer &operator=(const map_buffer &) = delete;
+};
+
+template<class K, class V, class Hash = std::hash<K>>
+class insertion_ordered_map {
+    map_buffer<K, V, Hash> *data;
+public:
+
+};
+
+
+#endif //INSERTION_ORDERED_MAP_INSERTION_ORDERED_MAP_H
+
